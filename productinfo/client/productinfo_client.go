ac64b27dff9848bf693dd9978c235e5f62e2d2a5
lhj8390 <lhj8390@naver.com>
2023-04-26 19:55:53 +0900

    productinfo client 코드 작성

---
+++ productinfo/client/productinfo_client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	pb "productinfo/client/ecommerce"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProductInfoClient(conn)
+
+	name := "Apple iPhone 11"
+	description := "Meet Apple iPhone 11."
+	price := float32(1000.0)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.AddProduct(ctx, &pb.Product{Name: name, Description: description, Price: price})
+	if err != nil {
+		log.Fatalf("Could not add product: %v", err)
+	}
+	log.Printf("Product ID: %s added successfully", r.Value)
+
+	product, err := c.GetProduct(ctx, &pb.ProductId{Value: r.Value})
+	if err != nil {
+		log.Fatalf("Could not get product: %v", err)
+	}
+	log.Printf("Product: %s", product.String())
+}
